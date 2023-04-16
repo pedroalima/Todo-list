7c11df8580dc213cf059bbf96513e7b168f88af1
Pedro Lima <85854788+pedroalima@users.noreply.github.com>
2023-04-16 16:28:15 -0300

    feat element creation update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 // Variáveis
 const todoForm = document.querySelector("#todoForm")
-const todoInput = document.querySelector('#todoInput');
+const todoInput = document.querySelector("#todoInput");
+const todoList = document.querySelector(".todo__list")
 
 // Funções
 const saveTodo = (text) => {
@@ -8,11 +9,21 @@ const saveTodo = (text) => {
     todo.classList.add("todo__item");
 
     const todoCheckbox = document.createElement("input");
+    todoCheckbox.setAttribute("type", "checkbox");
+    todoCheckbox.setAttribute("id", "todo-item5");
     todo.appendChild(todoCheckbox);
 
     const todoLabel = document.createElement("label");
+    todoLabel.htmlFor = "todo-item5";
     todo.appendChild(todoLabel);
     todoLabel.innerText = text;
+
+    const todoIcon = document.createElement("i");
+    todoIcon.innerHTML = '<img src="./assets/img/icon-cross.svg" alt="Icone em formato de X">';
+    todo.appendChild(todoIcon);
+
+    const todoFooter = document.querySelector(".todo__item-footer");
+    todoList.insertBefore(todo, todoFooter);
 
     console.log(todo);
 }
